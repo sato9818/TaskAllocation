d908bb2e42552a37267bb47f6cbb3a5318b2ecd1
Koki Sato <k.sato@SugawaraLab-MacbookPro-12.local>
2022-10-25 23:07:19 +0900

    add script rational

---
+++ scripts/taskallocation_with_rational.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python3 python/LineAlert.py start
+javac -sourcepath src -d bin src/Main.java
+java -cp bin Main RATIONAL
+
+python3 python/csvToGragh_area.py 
+python3 python/CsvToGraghAllWithoutCnp.py
+python3 python/LineAlert.py finish
